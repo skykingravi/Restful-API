4c73c9c752c395f8ea88bb340a99b03506606375
Ravi Meena <97457969+skykingravi@users.noreply.github.com>
2023-04-20 12:22:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // REST endpoint
 app.get('/', (req, res) => {
     
@@ -12,4 +19,6 @@ app.get('/', (req, res) => {
 
 });
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
