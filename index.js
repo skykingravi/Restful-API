1692d411514f01e6267e788415e21e68cc1e7de7
Ravi Meena <97457969+skykingravi@users.noreply.github.com>
2023-04-20 12:33:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
