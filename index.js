980533edf669ef60344f84d79f1c6175f4a28159
Ravi Meena <97457969+skykingravi@users.noreply.github.com>
2023-05-09 13:58:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     
     // for 'OK' response
     res.status(200).send({
-        color: `#${Math.floor(Math.random() * 0x1000000).toString(16)}`
+        color: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6,"0")}`
     });
 
 });
